618d1eebbffb645a8579bf3ce934b41edbfdf4ae
Paula Rodríguez <paularb923@hotmail.com>
2022-08-10 15:19:15 +0100

    iteration 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import './App.css';
 import foods from './foods.json';
+import FoodBox from './components/FoodBox';
 
-const foodsArr = foods
-
+const foodsArr = foods;
 
 function App() {
-
-
-
   return (
-    <div>
-    <h2>Food list</h2>
-      {foodsArr.map ((eachFood, index) => {
+    <div className="App">
+      <h2>Food list</h2>
+      {foodsArr.map((eachFood, index) => {
         return (
-          <div>
+          <div key={(eachFood, index)}>
             <p>{eachFood.name}</p>
             <img src={eachFood.image} alt="food" width={100} />
           </div>
-        )
+        );
       })}
+
+      <FoodBox
+        food={{
+          name: 'Orange',
+          calories: 85,
+          image: 'https://i.imgur.com/abKGOcv.jpg',
+          servings: 1,
+        }}
+      />
     </div>
   );
 }
